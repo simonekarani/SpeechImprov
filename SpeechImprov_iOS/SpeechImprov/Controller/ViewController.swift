2b5b8687e6ee56769b2fd939afcf71f5bdefeb78
Simone Karani <simone.karani@gmail.com>
2020-01-12 16:59:04 -0800

    combine recording with playback

--- SpeechImprov_iOS/SpeechImprov/Controller/ViewController.swift
+++ SpeechImprov_iOS/SpeechImprov/Controller/ViewController.swift
@@ -12,28 +12,30 @@ import AVFoundation
 
 class ViewController: UIViewController, AVAudioPlayerDelegate {
 
-    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
+    var recordingSession: AVAudioSession!
+    var audioRecorder: AVAudioRecorder!
+    var numberOfRecords:Int = 0
+    var audioPlayer:AVAudioPlayer!
+    var recFilename:URL!
+
     @IBOutlet weak var transcriptionTextField: UITextView!
     
-    var audioPlayer:AVAudioPlayer!
+    @IBOutlet weak var recButton: CircleButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        activitySpinner.isHidden = true
     }
     
     func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
         player.stop()
-        activitySpinner.stopAnimating()
-        activitySpinner.isHidden = true
     }
     
     func requestSpeechAuth() {
         SFSpeechRecognizer.requestAuthorization { authStatus in
             if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
-                if let path = Bundle.main.url(forResource: "test", withExtension: "m4a") {
+                if self.recFilename != nil {
                     do {
-                        let sound = try AVAudioPlayer(contentsOf: path)
+                        let sound = try AVAudioPlayer(contentsOf: self.recFilename)
                         self.audioPlayer = sound
                         self.audioPlayer.delegate = self
                         sound.play()
@@ -42,7 +44,7 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
                     }
                     
                     let recognizer = SFSpeechRecognizer()
-                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    let request = SFSpeechURLRecognitionRequest(url: self.recFilename)
                     recognizer?.recognitionTask(with: request) { (result, error) in
                         if let error = error {
                             print("There was an error: \(error)")
@@ -55,9 +57,50 @@ class ViewController: UIViewController, AVAudioPlayerDelegate {
         }
     }
     
+    // function that gets path to diretory for recording file
+    func getDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = paths[0]
+        return documentDirectory
+    }
+
+    @IBAction func recordButtonPressed(_ sender: Any) {
+        // check if we have active recorder
+        if audioRecorder == nil {
+            numberOfRecords += 1
+            recFilename = getDirectory().appendingPathComponent("recording.m4a")
+            let settings = [AVFormatIDKey: Int(kAudioFormatMPEG4AAC), AVSampleRateKey: 12000, AVNumberOfChannelsKey: 1, AVEncoderAudioQualityKey: AVAudioQuality.high.rawValue]
+            
+            // start audio recording
+            do {
+                audioRecorder = try AVAudioRecorder(url: recFilename, settings: settings)
+                audioRecorder.delegate = self as? AVAudioRecorderDelegate
+                audioRecorder.record()
+                recButton.setTitle("Stop", for: .normal)
+                print(recFilename)
+                
+            } catch {
+                displayAlert(title: "Ops!", message: "Recording Failed. Please Try again...")
+            }
+        }
+        else {
+            // stop audio recording
+            audioRecorder.stop()
+            audioRecorder = nil
+            
+            recButton.setTitle("Rec", for: .normal)
+            UserDefaults.standard.set(numberOfRecords, forKey: "audioRecordingCount")
+        }
+    }
+
+    // function that displays an alert
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
     @IBAction func playButtonPressed(_ sender: Any) {
-        activitySpinner.isHidden = false
-        activitySpinner.startAnimating()
         requestSpeechAuth()
     }
 }
